989dbd05de2eca8209df0bcf7b13cfd998756850
jason.melis@gmail.com <jason.melis@gmail.com>
2024-01-02 17:03:13 +0000

    Created Supabase; Edited Form2 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.tables as tables
@@ -13,3 +14,4 @@ class Form1(Form1Template):
     self.init_components(**properties)
     anvil.users.login_with_form()
     # Any code you write here will run before the form opens.
+    self.drop_down_1.items = anvil.server.call('getBckets')
