be61580b78532e9f0463cfc954c462ddb379ff2e
jason.melis@gmail.com <jason.melis@gmail.com>
2024-01-07 16:37:18 +0000

    Created Home.Settings.RowTemplate1; Edited Home.Settings

--- client_code/Home/Settings/RowTemplate1/__init__.py
+++ client_code/Home/Settings/RowTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import SettingsTemplate
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,10 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Settings(SettingsTemplate):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.repeating_panel_buckets.items = [{"name": "test", "target": 200}, {"name": "test", "target": 200}, {"name": "test", "target": 200}]
